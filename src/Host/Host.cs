eff3803d4fd57dfce44b9776fc63859b932f094a
Martin Zubovskiy <118899868+marz1010111@users.noreply.github.com>
2023-10-12 20:20:59 +0300

    updated

--- src/Host/Host.cs
+++ src/Host/Host.cs
@@ -6,7 +6,30 @@ namespace Host
     {
         static void Main(string[] arg)
         {
-            
+
+            Account<long> myAccount = new Account<long>();
+            myAccount.Id = 100L;
+
+            int idLocal = (int)(long)myAccount.Id; // в лонг, а потом в инт
+
+
+
+            /*try
+            {
+                throw new InvalidOperationException("Do not know what to do");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+
+                //throw; // на рус -> сроу
+                //throw new Exception("MyException"); // not good
+                //throw e; //not good
+
+                throw;
+            }
+
+            Console.WriteLine("FINISH");*/
         }
     }
 }
